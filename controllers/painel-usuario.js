7a1133cc199109b7ff2664f52baaf02ee6861f57
robcs9 <rcs9@protonmail.com>
2023-07-07 11:22:51 -0300

    Atualizando views do painel de usuario

--- controllers/painel-usuario.js
+++ controllers/painel-usuario.js
@@ -13,29 +13,57 @@ exports.getUsuario = (req, res, next) => {
         err => console.log(err)
     )
 }
-// Crumbs + Título da Tela
+// Breadcrumbs + Título da Tela
 exports.crumbsInicio = (req, res, next) => {
-    res.locals.breadcrumb = ["Painel"];
+    //res.locals.breadcrumb = ["Painel"];
+    res.locals.breadcrumb = [
+        {titulo: "Home", rota: "/"},
+        {titulo: "Painel", rota: "/painel-usuario"}
+    ];
     next();
 }
 
 exports.crumbsPerfil = (req, res, next) => {
-    res.locals.breadcrumb = ["Painel", "Perfil"];
+    //res.locals.breadcrumb = ["Painel", "Perfil"];
+    res.locals.breadcrumb = [
+        {titulo: "Home", rota: "/"},
+        {titulo: "Painel", rota: "/painel-usuario"},
+        {titulo: "Perfil", rota: "/perfil"}
+    ];
     next();
 }
 
 exports.crumbsPublicacoes = (req, res, next) => {
-    res.locals.breadcrumb = ["Painel", "Publicações"];
+    //res.locals.breadcrumb = ["Painel", "Publicações"];
+    res.locals.breadcrumb = [
+        {titulo: "Home", rota: "/"},
+        {titulo: "Painel", rota: "/painel-usuario"},
+        {titulo: "Publicações", rota: "/publicacoes"},
+    ];
     next();
 }
 
 exports.crumbsNovaPublicacao = (req, res, next) => {
-    res.locals.breadcrumb = ["Painel", "Perfil", "Publicações", "Criar Publicação"];
+    //res.locals.breadcrumb = ["Painel", "Perfil", "Publicações", "Criar Publicação"];
+    res.locals.breadcrumb = [
+        {titulo: "Home", rota: "/"},
+        {titulo: "Painel", rota: "/painel-usuario"},
+        {titulo: "Perfil", rota: "/perfil"},
+        {titulo: "Publicações", rota: "/publicacoes"},
+        {titulo: "Criar Publicação", rota: "/nova-publicacao"},
+    ];
     next();
 }
 
 exports.crumbsEditarPublicacao = (req, res, next) => {
-    res.locals.breadcrumb = ["Painel", "Perfil", "Publicações", "Editar Publicação"];
+    //res.locals.breadcrumb = ["Painel", "Perfil", "Publicações", "Editar Publicação"];
+    res.locals.breadcrumb = [
+        {titulo: "Home", rota: "/"},
+        {titulo: "Painel", rota: "/painel-usuario"},
+        {titulo: "Perfil", rota: "/perfil"},
+        {titulo: "Publicações", rota: "/publicacoes"},
+        {titulo: "Editar Publicação", rota: "/editar-publicacao"},
+    ];
     next();
 }
 
